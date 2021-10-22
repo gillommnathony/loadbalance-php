4f2fce92df68fedaf5c6435a0e92aff9b3afb49b
advuianime <68636322+advuianime@users.noreply.github.com>
2021-10-21 22:04:46 -0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ define('BASE_URL', 'https://cdbpj.herokuapp.com/');
 
 // BASE_DIR, format: string, example: '/home/user/public_html/'
 // adjust to your website directory (slash sign at the end)
-define('BASE_DIR', '/www/wwwroot/humanpx.live/');
+//define('BASE_DIR', '/www/wwwroot/humanpx.live/');
 
 // SECURE_SALT, format: string, example: 'kmzwayw1aa-12345'
 define('SECURE_SALT', 'password-123');
